fa788421443a2a8ab6b65f25fa6c23c7f92de881
Nikhil Bodicharla <nbodicha@gmail.com>
2020-10-09 01:43:16 -0500

    added stuff

--- COVID_Gene_Expression_Analysis.R
+++ COVID_Gene_Expression_Analysis.R
@@ -1,21 +1,40 @@
 library(data.table)
 library(dplyr)
+library(DESeq2)
+library(Hmisc)
 
-file_path <- '/Users/administrator/Documents/Classes/Introduction_to_Computational_Systems_Biology/COVID_Gene_Expression_Analysis/expression_data.tsv'
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(version = "3.11")
+
+file_path <- '/Users/nikhilbodicharla/Downloads/GeneData.tsv'
 genes = row.names(read.table(file_path, sep='\t', header = FALSE, row.names=1))
 
 target_genes = c('NUP98', 'RAE1', 'FN1', 'AHCTF1', 'NUP35', 'NUP160', 'NUP37', 'SEH1L')
-
-# Non-COVID non-ICU expression data
 non_covid_patients <- c('NC4', 'NC5', 'NC9', 'NC11', 'NC21', 'NC22', 'NC23', 'NC24', 'NC25')
-non_covid_expression_data = fread(file_path, sep='\t', header = TRUE, data.table = FALSE, select = non_covid_patients)
-row.names(non_covid_expression_data) <- genes
-non_covid_expression_data = non_covid_expression_data[target_genes,]
-
-# COVID ICU expression data
 covid_icu_patients <- c('C8', 'C9', 'C10', 'C12', 'C14', 'C15', 'C20', 'C21', 'C22', 'C23', 'C24', 'C25', 'C26', 'C27', 'C28', 'C29', 'C30', 'C31', 'C32', 'C38', 'C39', 'C40', 'C41', 'C42', 'C43', 'C44', 'C45', 'C46', 'C47', 'C48', 'C49', 'C50', 'C55', 'C62', 'C63', 'C64', 'C67', 'C68', 'C71', 'C74', 'C76', 'C77', 'C80', 'C85', 'C87', 'C92', 'C93', 'C97', 'C99', 'C101')
-covid_icu_expression_data = fread(file_path, sep='\t', header = TRUE, data.table = FALSE, select = covid_icu_patients)
-row.names(covid_icu_expression_data) <- genes
-covid_icu_expression_data = covid_icu_expression_data[target_genes,]
+all_patients = c(as.character(covid_icu_patients), as.character(non_covid_patients))
+
+expression_data = fread(file_path, sep='\t', header = TRUE, data.table = FALSE, select = all_patients)
+row.names(expression_data) <- genes
+condition <- c('Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid',
+               'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid',
+               'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid' ,
+               'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid' ,
+               'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid', 'Covid' ,
+               'Control', 'Control', 'Control', 'Control', 'Control', 'Control', 'Control', 'Control', 'Control')  #vector of column names for the data frame
+colData <- data.frame(row.names=colnames(all_patients), condition=factor(condition, levels=c('Control','Covid')))
+
+dataset <- DESeqDataSetFromMatrix(countData = expression_data,
+                                  colData = colData,
+                                  design = ~condition)
+dds <- DESeq(dataset)
+result <- results(dds, contrast=c('condition','Covid','Control'))
+result <- result[complete.cases(result),] 
+
+
+
+
+
 
