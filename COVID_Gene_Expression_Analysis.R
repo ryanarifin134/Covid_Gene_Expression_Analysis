21d704089d138eceb3bd14db630b3eb284c302d8
Ryan Arifin <ryanarifin134@gmail.com>
2020-10-09 00:03:35 -0500

    Filter to target genes

--- COVID_Gene_Expression_Analysis.R
+++ COVID_Gene_Expression_Analysis.R
@@ -3,18 +3,19 @@ library(dplyr)
 
 file_path <- '/Users/administrator/Documents/Classes/Introduction_to_Computational_Systems_Biology/COVID_Gene_Expression_Analysis/expression_data.tsv'
 
-
 genes = row.names(read.table(file_path, sep='\t', header = FALSE, row.names=1))
+
+target_genes = c('NUP98', 'RAE1', 'FN1', 'AHCTF1', 'NUP35', 'NUP160', 'NUP37', 'SEH1L')
 
 # Non-COVID non-ICU expression data
 non_covid_patients <- c('NC4', 'NC5', 'NC9', 'NC11', 'NC21', 'NC22', 'NC23', 'NC24', 'NC25')
 non_covid_expression_data = fread(file_path, sep='\t', header = TRUE, data.table = FALSE, select = non_covid_patients)
 row.names(non_covid_expression_data) <- genes
+non_covid_expression_data = non_covid_expression_data[target_genes,]
 
 # COVID ICU expression data
 covid_icu_patients <- c('C8', 'C9', 'C10', 'C12', 'C14', 'C15', 'C20', 'C21', 'C22', 'C23', 'C24', 'C25', 'C26', 'C27', 'C28', 'C29', 'C30', 'C31', 'C32', 'C38', 'C39', 'C40', 'C41', 'C42', 'C43', 'C44', 'C45', 'C46', 'C47', 'C48', 'C49', 'C50', 'C55', 'C62', 'C63', 'C64', 'C67', 'C68', 'C71', 'C74', 'C76', 'C77', 'C80', 'C85', 'C87', 'C92', 'C93', 'C97', 'C99', 'C101')
-covid_expression_data = fread(file_path, sep='\t', header = TRUE, data.table = FALSE, select = covid_icu_patients)
-row.names(covid_expression_data) <- genes
-
-print('cat')
+covid_icu_expression_data = fread(file_path, sep='\t', header = TRUE, data.table = FALSE, select = covid_icu_patients)
+row.names(covid_icu_expression_data) <- genes
+covid_icu_expression_data = covid_icu_expression_data[target_genes,]
 
